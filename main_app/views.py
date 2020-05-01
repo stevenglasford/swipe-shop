f83ada72ed6eb3b4edad20fb407bca9f0bc56018
ben.harvey1213 <ben.harvey1213@gmail.com>
2020-04-30 21:54:05 -0500

    initialized pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'main_app/main_page/index.html')
+    
+def profile(request):
+    return render(request, 'main_app/profile/profile.html')
